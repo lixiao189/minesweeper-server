b1684542bbc3374b5607fca9ab2b2eb0edd8819b
I_Info <49438478+I-Info@users.noreply.github.com>
2021-06-18 15:17:22 +0800

    jar package fixed

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 mvn compile
 cd target/classes
-jar --create --file ../out.jar  --main-class team.minesweeper.service.App -v -C . $(find team -name "*.class")
-cd ..
+jar --create --file ../out.jar  --main-class team.minesweeper.service.App -v -C . .
+cd ../..
 echo "build finished"
